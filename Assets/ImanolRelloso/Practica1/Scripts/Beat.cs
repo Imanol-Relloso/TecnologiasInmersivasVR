5319f498ef75dd7dc61c604d559dfbe98b83eb59
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-03-18 20:20:56 +0100

    si

--- Assets/ImanolRelloso/Practica1/Scripts/Beat.cs
+++ Assets/ImanolRelloso/Practica1/Scripts/Beat.cs
@@ -6,4 +6,19 @@ public class Beat : MonoBehaviour
     {
         //transform.GetComponent<Rigidbody>().AddForce(Vector3.up);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+
+        if (other.transform.GetComponent<Palo>())
+        {
+            destroy(other.transform);
+        }
+    }
+    private void destroy(Transform beat)
+    {
+        BeatManager.instance.addPoints();
+        Destroy(beat);
+    }
 }
--- Assets/ImanolRelloso/Practica1/Scripts/Palo.cs
+++ Assets/ImanolRelloso/Practica1/Scripts/Palo.cs
@@ -3,18 +3,5 @@ using UnityEngine;
 
 public class Palo : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.gameObject);
 
-        if (other.transform.GetComponent<Beat>())
-        {
-            Beat(other.transform);
-        }
-    }
-    private void Beat(Transform beat)
-    {
-        BeatManager.instance.addPoints();
-        Destroy(beat);
-    }
 }
